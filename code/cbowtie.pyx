8a860053ef59cea8cd8fcc4970419581f39a19d0
brentp <bpederse@gmail.com>
2010-03-11 17:05:04 -0800

    add debugging stuff to cython counter

--- code/cbowtie.pyx
+++ code/cbowtie.pyx
@@ -29,12 +29,24 @@ def _update_conversions(char *ref_seq, char *aln_seq, int base_position,
             allowed_mismatches -= 1
     if allowed_mismatches < 0: return 1
 
+    # for proofing.
+    """
+    sys.stderr.write("ref:" + ref_seq + "\n")
+    sys.stderr.write("aln:" + aln_seq + "\n")
+    d = ["."] * n
+    """
+
     for i in range(n):
         a = ref_seq[i]
         if a != c1: continue
         b = aln_seq[i]
         # CC
-        if b == c1: c_count[base_position + i] = 1
+        if b == c1:
+            c_count[base_position + i] = 1
+            # d[i] = chr(c1)
         # CT
-        if b == c2: t_count[base_position + i] = 1
+        if b == c2:
+            t_count[base_position + i] = 1
+            # d[i] = chr(c2)
+    # sys.stderr.write("mat:" + "".join(d) + "\n")
     return 0
