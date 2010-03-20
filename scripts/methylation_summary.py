65ce4df8b11db78fc6c7d8d2a2ce69d88deb4689
brentp <bpederse@gmail.com>
2010-03-20 12:38:01 -0700

    add genome-wide summary.

--- scripts/methylation_summary.py
+++ scripts/methylation_summary.py
@@ -3,12 +3,15 @@ import os.path as op
 sys.path.insert(0, op.join(op.dirname(__file__), "../code"))
 from methyl import MethylGroup
 mg = MethylGroup(sys.argv[1])
+contexts = ('CG', 'CHG', 'CHH')
  
 print "#", mg.prefix, mg.dir, mg.pattern
  
+total_cs = dict((ctx, 0) for ctx in contexts)
+total_ts = dict((ctx, 0) for ctx in contexts)
 print "seqid,context,p_methylated,total_possible_sites,possible_sites_covered_by_reads,cs,ts,cs/(cs + ts)"
 for seqid, meth in mg.iteritems():
-    for context in ('CG', 'CHG', 'CHH'):
+    for context in contexts:
         if seqid in 'CcMm': continue
         cg_cs, cg_ts, cg_mask = meth.as_context(context)
         total_sites = cg_mask.sum()
@@ -22,6 +25,16 @@ for seqid, meth in mg.iteritems():
         proportion_methylated = float(n_methylated) / possible_methylated
         rat = float(cg_cs.sum())
         rat /= (rat + cg_ts.sum())
+        total_ts[context] += cg_ts.sum()
+        total_cs[context] += cg_cs.sum()
         rat = "%.5f" % rat
         proportion_methylated = "%.5f" % proportion_methylated
-        print ",".join(map(str, (seqid, context, proportion_methylated, total_sites, cg_mask.sum(), cg_cs.sum(), cg_ts.sum(), rat)))
+
+        print ",".join(map(str, (seqid, context, proportion_methylated, 
+                     total_sites, cg_mask.sum(), cg_cs.sum(), cg_ts.sum(), rat)))
+
+print '\n# genome wide methylation (c /(c + t))'
+for context in contexts:
+    cs = total_cs[context]
+    ts = total_ts[context]
+    print '# %s: %.4f'  % (context, cs / float(cs + ts))
