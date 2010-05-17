d25f2bcb011a1e82f9085a2e12ffd29951f2754f
brentp <bpederse@gmail.com>
2010-05-16 20:47:39 -0700

    add script to check fastq

---
+++ scripts/preprocess/check_fastq.py
@@ -0,0 +1,19 @@
+"""
+check a fastq file for validity checks are:
+   1) 4 line rrcords
+   2) header starts with @
+   3) qual line-length == seq line-length
+"""
+
+import sys
+
+fh = open(sys.argv[1])
+rl = fh.readline()
+while True:
+    header = rl().strip()
+    if not header: continue
+    assert header[0] == "@", (header)
+    seq = rl()
+    assert rl()[0] == "+"
+    qual = rl()
+    assert len(seq) == len(qual), (header, seq, qual)
