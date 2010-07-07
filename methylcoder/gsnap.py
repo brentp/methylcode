bf898264f75a7dbf9f4cf10ecff638c97e786ae9
brentp <bpederse@gmail.com>
2010-07-07 15:37:43 -0700

    gsnap (crackle, pop) fixes.

--- methylcoder/gsnap.py
+++ methylcoder/gsnap.py
@@ -1,17 +1,20 @@
 import sys
 import os.path as op
-from methylcoder import is_up_to_date_b, CPU_COUNT
+from methylcoder import is_up_to_date_b, CPU_COUNT, bin_paths_from_fasta, \
+        get_counts, _update_conversions, write_files
 from subprocess import Popen
+from pyfasta import Fasta
 
 # /opt/src/gmap/gmap-2010-03-09/util/gmap_setup -D ../ -d hg19e_gmap  ../hg19e.f
 def gmap_setup(gsnap_dir, out_dir, ref_fasta):
+    ref_fasta  = op.abspath(ref_fasta)
     ref_base = op.splitext(ref_fasta)[0]
     ref_name = op.basename(ref_base)
     make_path = "Makefile.%(ref_name)s" % locals()
     gsnap_setup = op.join(gsnap_dir, "util", "gmap_setup")
     out_dir = op.abspath(out_dir)
     # have to cd to the out_dir because gsnap writes to cwd.
-    cmd = "\n cd %(out_dir)s && "
+    cmd = "\n cd %(out_dir)s && \n"
     cmd += gsnap_setup
     cmd += " -D %(out_dir)s -o %(make_path)s -d %(ref_base)s %(ref_fasta)s > %(out_dir)s/gmap_setup.log && "
     cmd += "\n make -f %(make_path)s coords > gmap_coords.log && "
@@ -28,14 +31,13 @@ def gmap_setup(gsnap_dir, out_dir, ref_fasta):
         fh = open(cmd_last, "w")
         print >>fh, cmd
         fh.close()
-
-
-    if not (is_up_to_date_b(op.join(out_dir, make_path), cmd_last)):
-        print >>sys.stderr, "C"
+    if not op.exists(op.join(out_dir, make_path)) or not (is_up_to_date_b(op.join(out_dir, make_path), cmd_last)):
         # TODO ^ add check for cmetindex files as well.
         p = Popen(cmd.replace('\n', ' '), shell=True)
         print >>sys.stderr, "^ executing gmap/gsnap setup^"
         if p.wait() != 0:
+            pclean = Popen("cd %(out_dir)s; make -f %(make_path)s clean" % locals())
+            pclean.wait()
             pass
     else:
         print >>sys.stderr, "^ NOT executing gmap/gsnap setup. everything is up to date.^"
@@ -46,11 +48,12 @@ def run_gsnap(gsnap_dir, out_dir, ref_fasta, reads_fasta, cpu_count=CPU_COUNT):
     ref_base = op.splitext(ref_fasta)[0]
     ref_name = op.basename(ref_base)
     reads_fasta = op.abspath(reads_fasta)
+    log = op.join(out_dir, "gsnap_run.log")
 
     out_sam = op.abspath(op.join(out_dir, "methylcoder.gnsap.sam"))
     cmd = "%(gsnap_dir)s/src/gsnap --npaths 1 --quiet-if-excessive -A sam "
     cmd += " --nofails --nthreads %(cpu_count)i -D %(out_dir)s"
-    cmd += " -d %(ref_name)s --cmet %(reads_fasta)s > %(out_sam)s"
+    cmd += " -d %(ref_name)s --cmet %(reads_fasta)s > %(out_sam)s 2> %(log)s"
     cmd %= locals()
     cmd_path = op.join(out_dir, "ran_gsnap.sh")
     if not (op.exists(cmd_path) and open(cmd_path).read().strip() == cmd):
@@ -60,15 +63,16 @@ def run_gsnap(gsnap_dir, out_dir, ref_fasta, reads_fasta, cpu_count=CPU_COUNT):
     print >>sys.stderr, "\n" + cmd
     if is_up_to_date_b(reads_fasta, out_sam) and is_up_to_date_b(reads_fasta, cmd_path):
         print >>sys.stderr, "^ NOT executing gsnap. everything is up to date.^"
-        return out_sam
     else:
         print >>sys.stderr, "^ executing gsnap. ^"
         p = Popen(cmd.replace('\n', ' '), shell=True)
         p.wait()
+    return out_sam
 
 def fastq_to_gsnap_fasta(out_dir, reads):
     fa_name = out_dir + "/" + op.basename(reads[0]).rstrip("_1") + ".fasta"
-    if is_up_to_date_b(reads[0], fa_name):
+    if is_up_to_date_b(reads[0], fa_name) and is_up_to_date_b(reads[1], fa_name):
+        print >>sys.stderr, "using existing fasta"
         return fa_name
     out_fa = open(fa_name, "w")
 
@@ -93,7 +97,38 @@ def fastq_to_gsnap_fasta(out_dir, reads):
             read2(); read2()
     return fa_name
 
+def parse_gsnap_sam(gsnap_f, ref_path, out_dir):
+    fa = Fasta(ref_path)
+
+    fc, ft, fmethyltype = \
+            bin_paths_from_fasta(fa.fasta_name, out_dir)
+    counts = get_counts(fc, ft, fa)
+    print >>sys.stderr, "tabulating methylation"
+
+    for sline in open(gsnap_f):
+        if sline.startswith("@SQ"): continue
+        # the ends didn't map to same spot.
+        if not "=" in sline: continue
+
+        line = sline.split("\t")
+        if not line[6] == "=": continue
+        seqid = line[2]
+        sam_flag = int(line[1])
+        aln_seq = line[9]
+        bp0 = int(line[3])
+        end = int(line[7])
+        bp1 = bp0 + len(aln_seq) - 1
+        ga = ((sam_flag & 16) != 0) ^ (sam_flag & 128 != 0)
+
+        ref_seq = (fa[seqid][bp0 - 1:bp1]).upper()
+        letters = 'GA' if ga else 'CT'
+
+        _update_conversions(ref_seq, aln_seq, bp0, letters, counts[seqid]['c'], counts[seqid]['t'], 5, len(ref_seq))
+    write_files(fa.fasta_name, out_dir, counts)
+
 def main(out_dir, ref_fasta, reads, gsnap_path):
     bsnap_fasta = fastq_to_gsnap_fasta(out_dir, reads)
     gmap_setup(gsnap_path, out_dir, ref_fasta)
-    run_gsnap(gsnap_path, out_dir, ref_fasta, bsnap_fasta, cpu_count=CPU_COUNT)
+    gsnap_sam = run_gsnap(gsnap_path, out_dir, ref_fasta, bsnap_fasta, cpu_count=CPU_COUNT)
+
+    parse_gsnap_sam(gsnap_sam, ref_fasta, out_dir)
