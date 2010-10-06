2fa2ed4c1d526042ffe2ace78324a5b9e328fe59
brentp <bpederse@gmail.com>
2010-10-06 14:52:03 -0700

    gnsap -> gsnap

--- methylcoder/gsnap.py
+++ methylcoder/gsnap.py
@@ -51,7 +51,7 @@ def run_gsnap(gsnap_dir, gsnap_args, out_dir, ref_fasta, reads_paths, cpu_count)
     log = op.join(out_dir, "gsnap_run.log")
 
     reads_paths_str = " ".join(reads_paths)
-    out_sam = op.abspath(op.join(out_dir, "methylcoder.gnsap.sam"))
+    out_sam = op.abspath(op.join(out_dir, "methylcoder.gsnap.sam"))
     cmd = "%(gsnap_dir)s/src/gsnap --npaths 1 --quiet-if-excessive -A sam"
     cmd += " --nofails --nthreads %(cpu_count)i -D %(ref_dir)s %(gsnap_args)s"
     cmd += " -d %(ref_name)s --cmet %(reads_paths_str)s > %(out_sam)s 2> %(log)s"
