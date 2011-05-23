4206c80e217cc9ab0547c9eb828a47eaa26b9194
Brent Pedersen <bpederse@gmail.com>
2011-05-23 07:03:00 -0600

    add reads and ref to versions.sh

--- bench/versions.sh
+++ bench/versions.sh
@@ -4,3 +4,9 @@ BSMAP_VERSION=2.0
 
 BOWTIE_VERSION=0.12.7
 GSNAP_VERSION=2011-03-28
+
+R1=reads/WT_endosperm_BS_seq_raw_batch-2.1.fastq.trim
+R2=reads/WT_endosperm_BS_seq_raw_batch-2.2.fastq.trim
+REF=reference/thaliana_v10.fasta
+
+PROCESSORS=1
