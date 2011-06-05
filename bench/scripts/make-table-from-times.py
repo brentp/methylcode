3365f675bb414384668bd51ca8079ca2fded1160
Brent Pedersen <bpederse@gmail.com>
2011-06-05 08:49:13 -0600

    use minutes and mb for time and memory.

--- bench/scripts/make-table-from-times.py
+++ bench/scripts/make-table-from-times.py
@@ -10,8 +10,12 @@ import itertools
 
 def tail(f):
     for line in open(f): pass
-    mem_kb, seconds = line.rstrip().split()
-    return int(mem_kb), float(seconds)
+    try:
+        mem_kb, seconds = line.rstrip().split()
+    except:
+        print >>sys.stderr, "skipping ", f, "bad memory/time info"
+        return None, None
+    return int(mem_kb) / 1024., float(seconds) / 60
 
 def get_name(f):
     # remove .time
@@ -20,18 +24,19 @@ def get_name(f):
 def get_count(group, directory):
     cf = glob.glob(op.join(directory, "%s*.count" % group))
     assert len(cf) == 1, (cf, group, directory)
-    return int(open(cf[0]).read())
+    return int(float(open(cf[0]).read()))
 
 
 def make_table(time_files, directory):
     tails = [(get_name(f), tail(f)) for f in time_files]
+    tails = [t for t in tails if not None in t[1]]
     longest = sorted(tails, key=lambda a: len(a[0]))[-1][0]
     llen = len(longest)
     sep = "=" * 19
     row_format = "%21s %" + str(llen) + "s %19s %19s %19s"
     header_string = "%s %s %s %s %s" % ("=" * 21, "=" * (llen + 3), sep, sep, sep)
     print header_string
-    print row_format % ("program", "process", "memory (KB)", "time (seconds)",
+    print row_format % ("program", "process", "memory (MB)", "time (minutes)",
                                                                 "pairs-mapped")
     print header_string
     for group, li in itertools.groupby(sorted(tails), lambda a: a[0].split(".")[0]):
@@ -41,7 +46,7 @@ def make_table(time_files, directory):
         count = get_count(group, directory) if len(li) == 1 else ""
         for name, (mem, seconds) in li:
             name = "-".join(name.split("-")[-2:])
-            print row_format % (group, name, "%s" % mem, "%.1f" % seconds,
+            print row_format % (group, name, "%1.f" % mem, "%.1f" % seconds,
                     str(count))
 
         count = get_count(group, directory)
