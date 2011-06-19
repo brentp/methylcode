ffeea5b4e784a34299a4141f9995723fdb4d3180
Brent Pedersen <bpederse@gmail.com>
2011-06-19 14:59:53 -0600

    fix adaptor stuff; thanks to Jinyan for a good test-case

--- bench/scripts/fastq_pair_filter.py
+++ bench/scripts/fastq_pair_filter.py
@@ -6,6 +6,8 @@ filter reads from paired fastq so that no unmatching reads remain.
 output files are pair_1.fastq.trim and pair_2.fastq.trim
 see: http://hackmap.blogspot.com/2010/09/filtering-paired-end-reads-high.html
 """
+__version__ = "0.1.0"
+
 from subprocess import Popen, PIPE
 import sys
 
@@ -14,14 +16,13 @@ FASTQ_QUALITY_TRIMMER="fastq_quality_trimmer"
 def gen_pairs(fha, fhb, min_len, fastq):
     def gen_headers(fastq):
         fq = open(fastq)
-        r = True
+        r = fq.readline().rstrip("\r\n")
         while r:
-            r = fq.readline().rstrip()[:-2]
             fq.readline()
             fq.readline()
             fq.readline()
-            yield r
-
+            yield r[:-2]
+            r = fq.readline().rstrip("\r\n")
     aread, bread = fha.readline, fhb.readline
     get_a = lambda: [aread().rstrip("\r\n") for i in range(4)]
     get_b = lambda: [bread().rstrip("\r\n") for i in range(4)]
@@ -33,18 +34,24 @@ def gen_pairs(fha, fhb, min_len, fastq):
         ah = a[0][:-2]
         b = get_b()
         bh = b[0][:-2]
+
+        while not header in (ah, bh):
+            header = header_gen.next()
+
         if bh != header:
-            while ah != bh:
+            while ah != bh and ah:
                 a = get_a()
                 ah = a[0][:-2]
             while header != bh:
                 header = header_gen.next()
         if ah != header:
-            while ah != bh:
+            while ah != bh and bh:
                 b = get_b()
                 bh = b[0][:-2]
             while header != bh:
                 header = header_gen.next()
+        if not ah and bh:
+            raise StopIteration
 
         assert ah == bh
         if len(a[1]) < min_len or len(b[1]) < min_len: continue
@@ -53,15 +60,21 @@ def gen_pairs(fha, fhb, min_len, fastq):
 def main(adaptors, M, t, min_len, fastqs, sanger=False):
     cmds = []
     for fastq in fastqs:
-        trim_cmd = "%s -t %i -l 0" % (FASTQ_QUALITY_TRIMMER, t)
-        if sanger: trim_cmd += " -Q 33"
-
-        clip_cmds = []
+        cmd = []
         for i, a in enumerate(adaptors):
-            clip_cmds.append("%s -a %s -M %i %s" \
-                 % (FASTX_CLIPPER, a, M, "-Q 33" if sanger else ""))
+            if M == 0:
+                matches = len(a)
+            else:
+                matches = min(M, len(a))
+            cmd.append("%s -a %s -M %i %s -l 0" \
+                 % (FASTX_CLIPPER, a, matches, "-Q 33" if sanger else "")) #, min_len))
 
-        cmds.append(" | ".join(clip_cmds + [trim_cmd]) + " < %s " % fastq)
+        trim_cmd = "%s -t %i -l 0" % (FASTQ_QUALITY_TRIMMER, t) #, min_len)
+        if sanger: trim_cmd += " -Q 33"
+        cmd.append(trim_cmd)
+        cmd[0] += " < %s" % fastq
+
+        cmds.append(" | ".join(cmd))
         print "[running]:", cmds[-1]
     procs = [Popen(cmd, stdout=PIPE, shell=True) for cmd in cmds]
 
@@ -90,8 +103,9 @@ if __name__ == "__main__":
     p.add_option("-a", dest="a", help="adaptor sequence to clip seperate multiples with ','", default="")
 
     p.add_option("-M", dest="M", help="require minimum adapter alignment length of N."
-               " If less than N nucleotides aligned with the adapter - don't clip it",
-                 default=4, type='int')
+               " If less than N nucleotides aligned with the adapter - don't clip it."
+               " default 0 means to require the full length of the adaptor to match. ",
+                 default=0, type='int')
 
     p.add_option("-t", dest="t", help="Quality threshold - nucleotides with lower"
                           " quality will be trimmed (from the end of the sequence ",
