3e6ecf61d76fed661a8780d606c6b37f0a40bfd4
Brent Pedersen <bpederse@gmail.com>
2011-11-28 19:24:22 -0700

    add start of simplest script to run gsnap and get methylation values

---
+++ bench/gsnap-meth.py
@@ -0,0 +1,104 @@
+"""
+runner for gsnap on methylation data.
+gmap_build, gsnap and samtools must be on the PATH of the calling environment
+"""
+
+"""
+gmap_build -k 12 -D /usr/local/src/methylcoder/bench/reference -d thaliana_v10 /usr/local/src/methylcoder/bench/reference/thaliana_v10.fasta > /usr/local/src/methylcoder/bench/methylcoder_gsnap/gmap_build.log && 
+cmetindex -d thaliana_v10 -F /usr/local/src/methylcoder/bench/reference > gmap_cmetindex.log 2> gmap_cmetindex.error.log
+
+gsnap --quiet-if-excessive -A sam -k 12  --nofails --nthreads 2 -D /usr/local/src/methylcoder/bench/reference --quiet-if-excessive --npaths 1 -d thaliana_v10 --mode=cmet-nonstranded /usr/local/src/methylcoder/bench/reads/WT_endosperm_BS_seq_raw_batch-2.1.fastq.trim /usr/local/src/methylcoder/bench/reads/WT_endosperm_BS_seq_raw_batch-2.2.fastq.trim > /usr/local/src/methylcoder/bench/methylcoder_gsnap/methylcoded.gsnap.sam 2> /usr/local/src/methylcoder/bench/methylcoder_gsnap/gsnap_run.log
+
+gsnap --quiet-if-excessive -A sam -k 12  --nofails --nthreads 2 -D /usr/local/src/methylcoder/bench/reference --quiet-if-excessive --npaths 1 -d thaliana_v10 --mode=cmet-nonstranded /usr/local/src/methylcoder/bench/reads/WT_endosperm_BS_seq_raw_batch-2.1.fastq.trim
+"""
+
+import argparse
+import sys
+import os
+import os.path as op
+import subprocess as sp
+
+def sh(cmd):
+    return sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+
+def gsnap_meth(reference, reads, out_dir, kmer=15, stranded=False, extra_args=""):
+    ref_dir = op.dirname(reference)
+    ref_base = op.splitext(op.basename(reference))[0] # locals
+    ref_name = op.basename(reference) # locals.
+    assert os.access(reference, os.R_OK), ("reference not found / readable")
+    assert os.access(ref_dir, os.W_OK), ("%s must be writable" % (ref_dir))
+
+    cmd = "gmap_build -k %(kmer)i -D %(ref_dir)s -d %(ref_base)s %(reference)s"
+    cmd %= locals()
+    print >>sys.stderr, cmd + "\n"
+
+    cmd_cmet = "cmetindex -d %(ref_base)s -F %(ref_dir)s -k %(kmer)d\n"
+    cmd_cmet %= locals()
+    print >>sys.stderr, cmd_cmet
+
+    threads = 2 # locals
+    mode = ["cmet-nonstranded", "cmet-stranded"][int(stranded)]
+    reads_str = " ".join(reads)
+    cmd_gsnap = "gsnap --npaths 1 --quiet-if-excessive --nthreads %(threads)s \
+        -A sam -k %(kmer)d -D %(ref_dir)s -d %(ref_base)s --mode %(mode)s \
+           %(reads_str)s"
+    cmd_gsnap += "| samtools view -bSF 4 - > %(out_dir)s/gsnap-meth.u.bam"
+    cmd_gsnap %= locals()
+    print >>sys.stderr, cmd_gsnap
+
+    cmd_sort = "samtools sort -m 400000 %(out_dir)s/gsnap-meth.u.bam \
+            %(out_dir)s/gsnap-meth\n" % locals()
+    print >>sys.stderr, cmd_sort
+
+    cmd_index = "samtools index %(out_dir)s/gsnap-meth.bam\n" % locals()
+    print >>sys.stderr, cmd_index
+
+    cmd_pileup = "samtools mpileup -f %(reference)s -ABI \
+            $(out_dir)s/gsnap-meth.bam\n" % locals()
+    print >>sys.stderr, cmd_pileup
+
+def run(args):
+
+    if not op.exists(args.out_dir): os.makedirs(args.out_dir)
+    reference, kmer = op.abspath(args.reference), args.kmer
+    reads = map(op.abspath, args.reads)
+    gsnap_meth(reference, reads, args.out_dir, args.kmer, args.stranded,
+               args.extra_args)
+
+
+
+
+
+
+def main():
+    p = argparse.ArgumentParser(description=__doc__,
+                   formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument("-r", "--reference", help="reference fasta")
+    p.add_argument("-k", dest="kmer", help="kmer length for gsnap. default:" \
+                   + "%(default)i", type=int, default=15)
+    p.add_argument("--stranded", action="store_true", default=False, help=\
+                   "by default, non-stranded library prep is assumed")
+    p.add_argument("--out-dir", dest="out_dir", help="path to output directory",
+                  default="gsnap-meth")
+    p.add_argument("--extra-args", dest="extra_args", help="extra arguments"
+                " to send to gsnap")
+
+    p.add_argument('reads', nargs='+', help='reads files (if 2 files are'
+                   'they are assumed to be paired end')
+
+    try:
+        args = p.parse_args()
+    except:
+        p.print_help()
+        raise
+
+    if (not len(args.reads) in (1, 2)) or args.reference is None:
+        sys.exit(not p.print_help())
+
+    run(args)
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod(optionflags=doctest.ELLIPSIS |\
+                                   doctest.NORMALIZE_WHITESPACE).failed == 0:
+        main()
